81c6cbac746c8fa491d8cce3893b02a4122b310a
Jakub Plichcinski <jakub.plichcinski@blocknox.de>
2025-09-28 21:06:03 +0200

    Bump version to 0.2.1

---
+++ scripts/run-typedoc.mjs
@@ -0,0 +1,21 @@
+import TypeDoc from 'typedoc';
+import fs from 'fs';
+
+(async () => {
+    const app = new TypeDoc.Application();
+    app.options.addReader(new TypeDoc.TSConfigReader());
+    app.options.addReader(new TypeDoc.TypeDocReader());
+    app.bootstrap({
+        options: JSON.parse(fs.readFileSync('./typedoc.json', 'utf8'))
+    });
+    const project = app.convert();
+    if (!project) {
+        console.error('TypeDoc: no project generated');
+        process.exit(1);
+    }
+    await app.generateDocs(project, './site/api').catch(err => {
+        console.error('generateDocs error', err);
+        process.exit(1);
+    });
+    console.log('TypeDoc finished');
+})();
